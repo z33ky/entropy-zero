046e2021fb772ca8f752ef11d51d141ff05e73d0
1upD <1upderek@gmail.com>
2019-07-20 16:38:35 +0000

    Entropy : Zero 2 project

--- sp/src/createentropyzeroproject.bat
+++ sp/src/createentropyzeroproject.bat
@@ -1,2 +1,3 @@
-devtools\bin\vpc.exe /hl2 /episodic /ez1 /v +everything /mksln everything.sln
+devtools\bin\vpc.exe /hl2 /episodic /ez1 /ez2 /v +everything /mksln everything_entropyzero.sln
 devtools\bin\vpc.exe /ez1 /v +game /mksln entropyzero.sln
+devtools\bin\vpc.exe /ez2 /v +game /mksln entropyzero2.sln
